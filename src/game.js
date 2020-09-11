06bec21375a186c317d411f44bb91fb9b996f782
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-12 01:18:52 +0300

    done

--- src/game.js
+++ src/game.js
@@ -3,28 +3,22 @@ var getRandomInt = function (max) {
   };
   var userScore = 0;
   var computerScore = 0;
-
+   
   var playGame = function (event) {
     var actions = ["Scissors", "Paper", "Rock"];
     var computer = actions[getRandomInt(3)];
   
     var user = document.querySelector("#user-choice").value;
 
-    // if(user.indexOf('Rock', 'Paper', 'Scissors') ===-1){
-    //   return alert ('Check your spelling, pleasee!');
-    // }; // checks if the user input is correct
-
     var result = "";
     
-    
-   
     if (computer === user) {
       result = "It's a Tie";
     } 
      else if (
       (computer === "Paper" && user === "Rock") ||
       (computer === "Rock" && user === "Scissors") ||
-      (computer === "Scissors" && user === "Paper")
+      (computer === "Scissors" && user === "Paper")  
     ) {
       result = "Computer wins";
       computerScore +=1;
@@ -33,11 +27,11 @@ var getRandomInt = function (max) {
       userScore +=1;
     }
     if (userScore === 5){
-      alert('Yeeey User was first to reach 5 points, you win!!!')
+      alert('Yeeey User was first to reach 5 points, you win!!!');
     }
     else if( computerScore ===5){
-      alert('Computer reached 5 points, You Lose!')
-    };
+      alert('Computer reached 5 points, You Lose!');
+    }
     
     document.getElementById("computer-choice").innerText = computer;
     document.querySelector("#result").innerText = result;
@@ -45,18 +39,35 @@ var getRandomInt = function (max) {
     document.getElementById("player-score").innerText = userScore;
     // show the hidden element
     document.querySelector("div").style.display = "block";
-  };
+    
+  }
+    // ##################   Reset the Game    ##########################
 
-  var resetGame = function(event){
-    userScore = 0;
-    computerScore = 0; 
-    document.querySelector("div").style.display = "none";
-    // Reset the Game 
+   var resetGame = function(event){
+     userScore = 0;
+     computerScore = 0; 
+     document.querySelector("div").style.display = "none";
+     document.getElementById("myForm").reset();
+     
+   }
+
+   // ############# Check if the user input is correct  ###############
+
+  var checkInput = function(event){
+    var actions = ["Scissors", "Paper", "Rock"];
+    var user = document.querySelector("#user-choice").value;
+    user = user[0].toUpperCase() + user.substring(1).toLowerCase(); //match UpperCase & LowerCase for user input
+    if (actions.includes(user)) {
+       return playGame();
+      }
+      else {
+        console.log(user);
+        return alert ('Check your spelling, pleasee!');
+      }
   }
   var playBtn = document.getElementById("play");
-  playBtn.addEventListener("click", playGame);
+  playBtn.addEventListener("click", checkInput);
+  
   var resetBtn = document.getElementById("reset");
   resetBtn.addEventListener("click", resetGame);
-  
-  console.log(playBtn);
   
