547e05bf0ebe201147e1735e5ddd5ff354fd5429
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-12 03:02:23 +0300

    done

---
+++ src/calculator.js
@@ -0,0 +1,35 @@
+
+
+var calculate = function (event){
+
+var firstNumber = parseInt(document.querySelector('#firstNumber').value);
+var secondNumber = parseInt(document.querySelector('#secondNumber').value);
+var operation = document.querySelector('#operation').value;
+var rezultat ;
+
+  if(operation === "add"){
+   rezultat = firstNumber + secondNumber;
+  }
+  else if(operation === "sub"){
+    rezultat = firstNumber - secondNumber;
+  }
+  else if(operation === "multiply"){
+    rezultat = firstNumber * secondNumber;
+  }
+  else if(operation === "divide"){
+    rezultat = firstNumber * secondNumber;
+  }
+  document.querySelector("#rezultat").innerText = rezultat;
+};
+
+var reset = function(event){
+    document.getElementById("myForm2").reset();
+}
+
+var playBtn = document.getElementById("Calculate");
+playBtn.addEventListener("click", calculate);
+
+
+var resetBtn = document.getElementById("resetBtn");
+playBtn.addEventListener("click", reset);
+
