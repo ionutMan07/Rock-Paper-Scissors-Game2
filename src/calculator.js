a226482b70c807c551c2add55b8cbc6d1abb5ed5
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-13 00:54:53 +0300

    done

--- src/calculator.js
+++ src/calculator.js
@@ -1,38 +1,29 @@
+var calculate = function (event) {
+  var firstNumber = parseInt(document.querySelector("#firstNumber").value);
+  var secondNumber = parseInt(document.querySelector("#secondNumber").value);
+  var operation = document.querySelector("#operation").value;
+  var rezultat;
 
-var calculate = function (event){
-
-var firstNumber = parseInt(document.querySelector('#firstNumber').value);
-var secondNumber = parseInt(document.querySelector('#secondNumber').value);
-var operation = document.querySelector('#operation').value;
-var rezultat;
-
-
-  if(operation === "add"){
-   rezultat = firstNumber + secondNumber;
-  }
-  else if(operation === "sub"){
+  if (operation === "add") {
+    rezultat = firstNumber + secondNumber;
+  } else if (operation === "sub") {
     rezultat = firstNumber - secondNumber;
-  }
-  else if(operation === "multiply"){
+  } else if (operation === "multiply") {
+    rezultat = firstNumber * secondNumber;
+  } else if (operation === "divide") {
     rezultat = firstNumber * secondNumber;
   }
-  else if(operation === "divide"){
-    rezultat = firstNumber * secondNumber;
-  }
-  document.querySelector("#rezultat").innerText = rezultat;
-}
+  document.querySelector("#rezultat").innerText = rezultat.toFixed(2);
+};
 
-var reset = function(event){
-    var noValue = ' ';
-    document.getElementById("myForm2").reset();
-    document.querySelector("#rezultat").innerText = noValue;
-
-}
+var reset = function (event) {
+  var noValue = " ";
+  document.getElementById("myForm2").reset();
+  document.querySelector("#rezultat").innerText = noValue;
+};
 
 var playBtn = document.getElementById("Calculate");
 playBtn.addEventListener("click", calculate);
 
-
- var resetBtn = document.getElementById("resetBtn");
- resetBtn.addEventListener("click", reset);
-
+var resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", reset);
